3c98cbfc7b53b0acf3ff06f8fd1f0d3d85512ec3
Daniel <daniel.beumer@devoteam.com>
2023-10-24 17:33:32 +0200

    feat added db server

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -55,9 +55,15 @@ export class DetailsComponent {
     email: new FormControl('')
   });
 
+  // constructor() {
+  //   const housingLocationId = Number(this.route.snapshot.params['id']);
+  //   this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+  // }
   constructor() {
-    const housingLocationId = Number(this.route.snapshot.params['id']);
-    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+    const housingLocationId = parseInt(this.route.snapshot.params['id'], 10);
+    this.housingService.getHousingLocationById(housingLocationId).then(housingLocation => {
+      this.housingLocation = housingLocation;
+    });
   }
   submitApplication() {
     this.housingService.submitApplication(
